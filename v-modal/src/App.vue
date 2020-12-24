4c14783414a28f778f8890d5efac779d00f9a069
Tomáš Reinert <reinerttomas@gmail.com>
2020-12-24 12:34:25 +0100

    modal

--- v-modal/src/App.vue
+++ v-modal/src/App.vue
@@ -1,10 +1,43 @@
 <template>
-  <h1>It works</h1>
+  <Navbar />
+
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-12 text-center">
+        <h1 class="m-5">{{ title }}</h1>
+        <button class="btn btn-primary" @click="$refs.modal.toggleModal()">
+          Modal
+        </button>
+      </div>
+    </div>
+  </div>
+
+  <teleport to="#modal">
+    <Modal ref="modal" title="Title">
+      <template v-slot:body>
+        <p class="m-0">Modal body text goes here.</p>
+      </template>
+      <template v-slot:footer>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </template>
+    </Modal>
+  </teleport>
 </template>
 
 <script>
+import Navbar from "@/components/Navbar.vue";
+import Modal from "@/components/Modal.vue";
+
 export default {
   name: "App",
-  components: {},
+  components: {
+    Navbar,
+    Modal,
+  },
+  data() {
+    return {
+      title: "This is awesome modal",
+    };
+  },
 };
 </script>
---
+++ v-modal/src/components/Modal.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="modal fade" v-if="show" :class="showClass" :style="showStyle">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title">{{ title }}</h5>
+          <button type="button" class="close" @click="toggleModal">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+          <slot name="body"></slot>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" @click="toggleModal">
+            Close
+          </button>
+          <slot name="footer"></slot>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="modal-backdrop fade" v-if="showClass" :class="showClass"></div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      require: true,
+    },
+  },
+  data() {
+    return {
+      show: false,
+    };
+  },
+  computed: {
+    showClass() {
+      return this.show ? "show" : "";
+    },
+    showStyle() {
+      return this.show ? "display: block" : "";
+    },
+  },
+  methods: {
+    toggleModal() {
+      this.show = !this.show;
+    },
+  },
+};
+</script>
